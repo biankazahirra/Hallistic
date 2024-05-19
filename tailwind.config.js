fbfef58cbe0bcc6ca311fbb5d92ff8fc4a779fa1
Arkaan16 <muhammadfaruqarkaan16@gmail.com>
2024-05-19 17:09:47 +0700

    login
    ini adalah login

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      padding: {
+        '110': '149px',
+        '100': '141px', // Custom padding
+        '174': '174px',
+        '160': '137px',
+      },
+      colors: {
+        'dla': '#073937', // Tambahkan warna dla dengan kode warna yang diinginkan
+        'dlr': '#588157',
+        'abu': '#636364'
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
+
